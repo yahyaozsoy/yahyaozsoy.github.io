94b0022165afb486e93771228a46a7374fcac8a8
YahyaOyunda <79515501+yahyaozsoy@users.noreply.github.com>
2022-01-26 01:02:27 +0300

    🎇✨NICE✨🎇

--- javascript/deneme.js
+++ javascript/deneme.js
@@ -1,23 +1,31 @@
+document.onscroll=function() {changeColor()}
 
-// var animateX = anime({
-//     targets: '.wave1',
-//     fill: "#72c1f8",
-//     autoplay: false
-// });
-
-document.onscroll=function(){
-
-    if(window.pageYOffset < 10){
-        wave1.setAttribute("fill","#eeeeee")
-        wave3.setAttribute("fill","#f6f6f6")
-        wave5.setAttribute("fill","#ffffff")
-        wave7.setAttribute("fill","#ffffff")
-        wave9.setAttribute("fill","#ffffff")
-    }else{
-        wave1.setAttribute("fill", "#8ecdf9")
+function changeColor() {
+    if(window.pageYOffset < 400){
+        wave1.setAttribute("fill","#8ecdf9")
         wave3.setAttribute("fill","#72c1f8")
         wave5.setAttribute("fill","#52b5f8")
         wave7.setAttribute("fill","#469dda")
         wave9.setAttribute("fill","#3986bc")
+    }else/* if(document.body.scrollTop > 15)*/{
+        wave1.setAttribute("fill","#78a8cf")
+        wave3.setAttribute("fill","#5b97c5")
+        wave5.setAttribute("fill","#3986bc")
+        wave7.setAttribute("fill","#3174a2")
+        wave9.setAttribute("fill","#296289")
     }
+    //else if(document.body.scrollTop < 15){
+    //     wave1.setAttribute("fill","#8ecdf9")
+    //     wave3.setAttribute("fill","#72c1f8")
+    //     wave5.setAttribute("fill","#52b5f8")
+    //     wave7.setAttribute("fill","#469dda")
+    //     wave9.setAttribute("fill","#3986bc")
+    // }else if(document.body.scrollTop < 20){
+    //     wave1.setAttribute("fill", "#8ecdf9")
+    //     wave3.setAttribute("fill","#72c1f8")
+    //     wave5.setAttribute("fill","#52b5f8")
+    //     wave7.setAttribute("fill","#469dda")
+    //     wave9.setAttribute("fill","#3986bc")
+    // }
+    
 }
