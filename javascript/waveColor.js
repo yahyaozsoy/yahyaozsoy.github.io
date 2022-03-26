33d5f8c09b764b5d46c3a196de0a6954a08865fc
YahyaOyunda <79515501+yahyaozsoy@users.noreply.github.com>
2022-03-26 13:34:53 +0300

    #FreeUkraine

--- javascript/waveColor.js
+++ javascript/waveColor.js
@@ -1,19 +1,21 @@
-document.onscroll=function() {changeColor()}
+//document.onscroll=function() {changeColor()}
+window.onload = Ukraine;
 
 function changeColor() {
-    if(window.pageYOffset < 400){
+    /*if(window.pageYOffset < 400){
         wave1.setAttribute("fill","#8ecdf9")
         wave3.setAttribute("fill","#72c1f8")
         wave5.setAttribute("fill","#52b5f8")
         wave7.setAttribute("fill","#469dda")
         wave9.setAttribute("fill","#3986bc")
-    }else/* if(document.body.scrollTop > 15)*/{
+    }else /*if(document.body.scrollTop > 15){
         wave1.setAttribute("fill","#78a8cf")
         wave3.setAttribute("fill","#5b97c5")
         wave5.setAttribute("fill","#3986bc")
         wave7.setAttribute("fill","#3174a2")
         wave9.setAttribute("fill","#296289")
-    }
+    }*/
+    
     //else if(document.body.scrollTop < 15){
     //     wave1.setAttribute("fill","#8ecdf9")
     //     wave3.setAttribute("fill","#72c1f8")
@@ -28,4 +30,14 @@ function changeColor() {
     //     wave9.setAttribute("fill","#3986bc")
     // }
     
+}
+
+function Ukraine(){
+    if(window.pageYOffset <= 0){
+        wave1.setAttribute("fill","#fee46c")
+        wave3.setAttribute("fill","#fedd46")
+        wave5.setAttribute("fill","#005bbb")
+        wave7.setAttribute("fill","#004fa2")
+        wave9.setAttribute("fill","#00448a")
+    }
 }
